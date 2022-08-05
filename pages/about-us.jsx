c2ca9435eb0e05cd00f4d305795822a2497de13f
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2022-08-05 22:07:12 +0700

    áudhasiodhasos

--- pages/about-us.jsx
+++ pages/about-us.jsx
@@ -21,6 +21,117 @@ const AboutUs = () => {
          <main style={{ height: 'auto' }}>
             <div className="about-us-banner__container">
                <div className="container-full">
+                  <svg
+                     width={336}
+                     height={310}
+                     viewBox="0 0 336 310"
+                     fill="none"
+                     xmlns="http://www.w3.org/2000/svg"
+                  >
+                     <g filter="url(#filter0_b_355_8528)">
+                        <path
+                           d="M192.676 40.959v81.966l71.299 41.006 71.298-41.006V40.959L263.975 0l-71.299 40.959z"
+                           fill="#fff"
+                           fillOpacity={0.05}
+                        />
+                        <path
+                           d="M263.975 162.778l-70.299-40.432V41.538l70.299-40.385 70.298 40.385v80.808l-70.298 40.432z"
+                           stroke="#fff"
+                           strokeWidth={2}
+                        />
+                     </g>
+                     <g filter="url(#filter1_b_355_8528)">
+                        <path
+                           d="M0 82.346v151.191l131.988 75.639 131.987-75.639V82.347L131.988 6.794 0 82.346z"
+                           fill="#fff"
+                           fillOpacity={0.17}
+                        />
+                        <path
+                           d="M131.988 308.6L.5 233.247V82.637L131.988 7.37l131.487 75.265v150.611L131.988 308.6z"
+                           stroke="#fff"
+                        />
+                     </g>
+                     <g opacity={0.5} filter="url(#filter2_b_355_8528)">
+                        <path
+                           d="M168.062 108.926v27.605l24.191 13.81 24.191-13.81v-27.605l-24.191-13.795-24.191 13.795z"
+                           fill="#fff"
+                           fillOpacity={0.05}
+                        />
+                        <path
+                           d="M192.253 149.19l-23.191-13.24v-26.443l23.191-13.225 23.191 13.225v26.443l-23.191 13.24z"
+                           stroke="#fff"
+                           strokeWidth={2}
+                        />
+                     </g>
+                     <defs>
+                        <filter
+                           id="filter0_b_355_8528"
+                           x={184.676}
+                           y={-8}
+                           width={158.598}
+                           height={179.931}
+                           filterUnits="userSpaceOnUse"
+                           colorInterpolationFilters="sRGB"
+                        >
+                           <feFlood floodOpacity={0} result="BackgroundImageFix" />
+                           <feGaussianBlur in="BackgroundImage" stdDeviation={4} />
+                           <feComposite
+                              in2="SourceAlpha"
+                              operator="in"
+                              result="effect1_backgroundBlur_355_8528"
+                           />
+                           <feBlend
+                              in="SourceGraphic"
+                              in2="effect1_backgroundBlur_355_8528"
+                              result="shape"
+                           />
+                        </filter>
+                        <filter
+                           id="filter1_b_355_8528"
+                           x={-8}
+                           y={-1.20496}
+                           width={279.977}
+                           height={318.381}
+                           filterUnits="userSpaceOnUse"
+                           colorInterpolationFilters="sRGB"
+                        >
+                           <feFlood floodOpacity={0} result="BackgroundImageFix" />
+                           <feGaussianBlur in="BackgroundImage" stdDeviation={4} />
+                           <feComposite
+                              in2="SourceAlpha"
+                              operator="in"
+                              result="effect1_backgroundBlur_355_8528"
+                           />
+                           <feBlend
+                              in="SourceGraphic"
+                              in2="effect1_backgroundBlur_355_8528"
+                              result="shape"
+                           />
+                        </filter>
+                        <filter
+                           id="filter2_b_355_8528"
+                           x={160.062}
+                           y={87.1311}
+                           width={64.3828}
+                           height={71.21}
+                           filterUnits="userSpaceOnUse"
+                           colorInterpolationFilters="sRGB"
+                        >
+                           <feFlood floodOpacity={0} result="BackgroundImageFix" />
+                           <feGaussianBlur in="BackgroundImage" stdDeviation={4} />
+                           <feComposite
+                              in2="SourceAlpha"
+                              operator="in"
+                              result="effect1_backgroundBlur_355_8528"
+                           />
+                           <feBlend
+                              in="SourceGraphic"
+                              in2="effect1_backgroundBlur_355_8528"
+                              result="shape"
+                           />
+                        </filter>
+                     </defs>
+                  </svg>
                   <div className="about-us-banner__inner">
                      <div className="about-us-banner__title">
                         <h2 data-aos="fade-up">About Us</h2>
